8a5a8071c63fcc23dd2a135eb311c15ba84ddede
MechaCoffin <143288694+MechaCoffin@users.noreply.github.com>
2023-08-28 13:50:53 -0700

    Controls Broken For Now

--- Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
+++ Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
@@ -14,14 +14,12 @@ public class Borders
     [HideInInspector] public float minX, maxX, minY, maxY;
 }
 
-public class PlayerMoving : MonoBehaviour {
+public class PlayerMoving : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public Transform mainCamera;
 
-    [Tooltip("offset from viewport borders for player's movement")]
-    public Borders borders;
-    Camera mainCamera;
-    bool controlIsActive = true; 
-
-    public static PlayerMoving instance; //unique instance of the script for easy access to the script
+    private Vector2 joystickInput;
 
     private void Awake()
     {
@@ -34,46 +32,33 @@ public class PlayerMoving : MonoBehaviour {
         mainCamera = Camera.main;
         ResizeBorders();                //setting 'Player's' moving borders deending on Viewport's size
     }
-
-    private void Update()
+	
+	private void Update()
     {
         if (controlIsActive)
         {
-#if UNITY_STANDALONE || UNITY_EDITOR    //if the current platform is not mobile, setting mouse handling 
-
-            if (Input.GetMouseButton(0)) //if mouse button was pressed       
-            {
-                Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //calculating mouse position in the worldspace
-                mousePosition.z = transform.position.z;
-                transform.position = Vector3.MoveTowards(transform.position, mousePosition, 30 * Time.deltaTime);
-            }
-#endif
-
-#if UNITY_IOS || UNITY_ANDROID //if current platform is mobile, 
-
-            if (Input.touchCount == 1) // if there is a touch
-            {
-                Touch touch = Input.touches[0];
-                Vector3 touchPosition = mainCamera.ScreenToWorldPoint(touch.position);  //calculating touch position in the world space
-                touchPosition.z = transform.position.z;
-                transform.position = Vector3.MoveTowards(transform.position, touchPosition, 30 * Time.deltaTime);
-            }
-#endif
-            transform.position = new Vector3    //if 'Player' crossed the movement borders, returning him back 
-                (
-                Mathf.Clamp(transform.position.x, borders.minX, borders.maxX),
-                Mathf.Clamp(transform.position.y, borders.minY, borders.maxY),
-                0
-                );
-        }
-    }
-
-    //setting 'Player's' movement borders according to Viewport size and defined offset
-    void ResizeBorders() 
+	
+	
+			if (isButtonPressed)
     {
-        borders.minX = mainCamera.ViewportToWorldPoint(Vector2.zero).x + borders.minXOffset;
-        borders.minY = mainCamera.ViewportToWorldPoint(Vector2.zero).y + borders.minYOffset;
-        borders.maxX = mainCamera.ViewportToWorldPoint(Vector2.right).x - borders.maxXOffset;
-        borders.maxY = mainCamera.ViewportToWorldPoint(Vector2.up).y - borders.maxYOffset;
-    }
-}
+		
+        // Get joystick input
+        joystickInput.x = Input.GetAxis("Horizontal");
+        joystickInput.y = Input.GetAxis("Vertical");
+
+        // Calculate joystick movement direction
+        Vector3 moveDirection = new Vector3(joystickInput.x, joystickInput.y, 0);
+
+        // Calculate joystick movement vector
+        Vector3 moveVector = moveDirection.normalized * moveSpeed * Time.deltaTime;
+
+        // Move the object based on joystick input
+        transform.Translate(moveVector);
+
+        // Keep the object's z position the same as the camera's z position
+        Vector3 newPosition = transform.position;
+        newPosition.z = mainCamera.position.z;
+        transform.position = newPosition;
+		}
+	}
+}		
